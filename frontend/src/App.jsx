2ec3ffc84020ab270fd51e583994e5971b0a2a1d
Darren <darrendexterthio@gmail.com>
2026-06-28 18:50:46 +0700

    fixed directory app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes} from "react-router"
-import { Home } from "./pages/home"
+import { Home } from "./pages/Home"
 import { NotFound } from "./pages/NotFound"
 
 function App() {
